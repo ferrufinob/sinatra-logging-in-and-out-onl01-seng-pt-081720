4267b519196132a99f1110eecd8279b1ee086efb
Brenda Ferrufino <ferrufinob@gmail.com>
2020-11-16 15:03:33 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    #find user in database based on their username
+    # user = User.find_by(:username => params[:username])
+    #if match, session[user_id] = user_id
+    #redirect to /account
+    #use erb to display users data on page
+    #if no match, render the error page
+    user = User.find_by(username: params["username"], password: params["password"])
+    if user
+    session[:user_id] = user.id
+     
+      redirect to '/account'
+    else 
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
---
+++ db/migrate/20201116175201_create_users_table.rb
@@ -0,0 +1,10 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |user|
+      user.string :username
+      user.string :password
+      user.decimal :balance
+  end
+end
+
+end
